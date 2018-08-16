8e05c3406b071a35b0eb078ba6eb5d7758116665
TomaszKotowski <tomaszk72@gmail.com>
2018-08-16 23:25:00 +0200

    correct mistake in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const webpack = requiere('webpack');
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
